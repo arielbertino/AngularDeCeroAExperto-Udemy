b14c97dc6951b45f6c41831056ed03f4d6ad0aac
arielbertino <eugenioingenio10@gmail.com>
2023-05-12 12:11:56 -0300

    Bases typescrpt - Seccion 3 Video 14

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+//import './topics/01-basic-types'
+import './topics/02-object-interface'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML= ` Hola mundo `;
+
+console.log('hola mundo');
