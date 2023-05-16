5be623039fa8ac71646164f7c0047df74ae87718
arielbertino <eugenioingenio10@gmail.com>
2023-05-16 14:51:51 -0300

    directivas de angular varias - modulos

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -8,4 +8,29 @@ import { Component } from '@angular/core';
 export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
+
+  get capitalizedName(){
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero():void{
+    this.name = 'Spiderman';
+  }
+
+  changeAge():void{
+    this.age = 25;
+  }
+
+  resetForm(){
+    this.name = 'ironman';
+    this.age = 45;
+
+    // document.querySelectorAll('h1')!.forEach( element => {
+    //   element.innerHTML = '<h1>Desde Angular</h1>'
+    // })
+  }
 }
