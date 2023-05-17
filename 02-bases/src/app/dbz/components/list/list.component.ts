3e926a19a5045d1c8b8ccf0b0bba8e61312507d4
arielbertino <eugenioingenio10@gmail.com>
2023-05-17 13:10:30 -0300

    Comunicacio entre componentes Input() y Output()
    ngFor ngClass ngModel
    
    Flujo completo de comunicacion - UUID

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'dbz-list',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      id: uuid(),
+      name: 'Trunks',
+      power: 10,
+    }
+  ]
+
+  // Mi solucion al ejercicio de emitir el id de character
+  deleteCharacterById(id : string | undefined):void{
+  //solucion del profesor para la emision de id del character
+  // deleteCharacterById(id? : string | undefined):void{
+    //solucion del profesor para la emision de id del character
+    // if( !id ) return
+    this.onDelete.emit(id);
+  }
 }
--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'dbz-main-page',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 
 export class MainPageComponent{
 
+  constructor(
+    // public dbzService: DbzService
+    private dbzService: DbzService
+  ){ }
+
+  // A partir de aca comienza la solucion para mantene
+  // nuestro servicio privado para mayor control y seguridad del mismo
+
+  get characters(): Character[]{
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter( id: string):void{
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter ( character: Character): void{
+    this.dbzService.addCharacter(character);
+  }
+
+
 }
+
