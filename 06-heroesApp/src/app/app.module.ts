e3d524e17ee48d9fc14abb484405a3b716546eef
arielbertino <eugenioingenio10@gmail.com>
2023-06-27 16:46:06 -0300

    Usando Angular Material - HeroesApp

--- 06-heroesApp/src/app/app.module.ts
+++ 06-heroesApp/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -10,8 +11,9 @@ import { SharedModule } from './shared/shared.module';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
     SharedModule,
   ],
   providers: [],
--- 06-heroesApp/src/app/auth/auth.module.ts
+++ 06-heroesApp/src/app/auth/auth.module.ts
@@ -1,6 +1,8 @@
+import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { LayoutPageComponent } from '../heroes/pages/layout-page/layout-page.component';
@@ -15,6 +17,7 @@ import { LayoutPageComponent } from '../heroes/pages/layout-page/layout-page.com
   imports: [
     CommonModule,
     AuthRoutingModule,
+    MaterialModule,
   ],
 })
 export class AuthModule { }
