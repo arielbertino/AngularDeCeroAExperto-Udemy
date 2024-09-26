d131b801d19bf502a02bc8a207c0b534b1a51504
Ariel Bertino <zextabertino@correoargentino.com.ar>
2024-09-26 16:30:00 -0300

    Formularios reacctivos 1

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,76 @@
-import { Component } from '@angular/core';
-import { JsonPipe as json } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, JsonPipe as json } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+}
+
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``,
   standalone: true,
-  imports:[json]
+  imports:[
+    json,
+    ReactiveFormsModule,
+    CommonModule,
+  ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
+  // public myForm: FormGroup = new FormGroup ({
+  //   name : new FormControl(''),
+  //   price : new FormControl(0),
+  //   inStorage : new FormControl(0),
+  // });
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ] ],
+    price: [0, [Validators.required, Validators.min(0)] ],
+    inStorage: [0, [Validators.required, Validators.min(0)] ],
+  });
+
+  constructor ( private fb: FormBuilder ){}
+
+  ngOnInit(): void {
+    // this.myForm.reset( rtx5090 );
+  }
+
+  isValidField( field: string ){
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string): string | null {
+    if ( !this.myForm.controls[field] ) return null;
+    const errors = this.myForm.controls[field].errors || {};
+    for ( const key of Object.keys(errors) ) {
+      switch (key){
+        case 'required':
+          return 'Este campo es requerido'
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracteres`
+
+          'Este campo es requerido'
+      }
+      console.log(key);
+
+    }
+    return null;
+  }
+
+  onSave(){
+    if (this.myForm.invalid){
+      this.myForm.markAllAsTouched
+      return
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset(
+      {
+        price: 10,
+        inStorage: 10
+      }
+    );
+  }
 }
